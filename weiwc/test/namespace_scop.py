da508f59dfb1337d00a0bf80dc297f54977c260e
weiwc <972237007@qq.com>
2017-07-06 23:19:32 +0800

    增加namespace和scope的测试代码

---
+++ weiwc/test/namespace_scop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: python2.7
+@author: weiwc
+@license: Apache Licence 
+@contact: 972237007@qq.com
+@site: https://github.com/abelweiwencai
+@software: PyCharm Community Edition
+@file: namespace_scop.py
+@time: 2017/7/6 21:52
+"""
+
+
+def ot():
+    a = 0
+    b = 1
+    def ine():
+        print a,b
+        a = 1
+    ine()
+
+ot()
