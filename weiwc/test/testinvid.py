baae99dca40c1e19140c3f1f21518a078293d102
weiwcvid <972237007@qq.com>
2017-07-10 15:27:58 +0800

    vid add

---
+++ weiwc/test/testinvid.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+
+import weakref
+
+a = {1:1}
+b = weakref.WeakValueDictionary(a)
--- weiwc/weiwc/settings.py
+++ weiwc/weiwc/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'weiwc.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'weiwc',
-        'USER': 'weiwc',
-        'PASSWORD': 'weiwc',
-        'HOST': "localhost",
-        'PORT': "3306",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'weiwc',
+        # 'USER': 'weiwc',
+        # 'PASSWORD': 'weiwc',
+        # 'HOST': "localhost",
+        # 'PORT': "3306",
     }
 }
 
