0993bed39bfeb621bcbcd7ae940eaea53f9d08b8
vidweiwc <972237007@qq.com>
2017-07-14 18:11:53 +0800

    提交

--- weiwc/polls/views.py
+++ weiwc/polls/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+
 from . import models
+from .models import Question, Choice
 
 from django.template import loader
 
@@ -29,8 +33,39 @@ def detail(request, question_id):
     return render(request, 'polls/details.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse(u"you are looking at the results of {rid}!".format(rid=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {"question": question,})
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/details.html"
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse(u"you are voting on {rid}".format(rid=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html',
+                      {'question': question, 'error_message': 'You dont habe select a choice'})
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
