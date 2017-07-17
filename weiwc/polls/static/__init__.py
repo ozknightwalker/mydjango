85508a963c0d9aee8121596be534274683111ecc
weiwc <972237007@qq.com>
2017-07-17 08:06:01 +0800

    add test

---
+++ weiwc/polls/static/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: python2.7
+@author: weiwc
+@license: Apache Licence 
+@contact: 972237007@qq.com
+@site: https://github.com/abelweiwencai
+@software: PyCharm Community Edition
+@file: __init__.py.py
+@time: 2017/7/16 14:00
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- weiwc/polls/tests.py
+++ weiwc/polls/tests.py
@@ -1,6 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#!/usr/bin/env python
+# encoding: utf-8
 
+"""
+@version: python2.7
+@author: weiwc
+@license: Apache Licence 
+@contact: 972237007@qq.com
+@site: https://github.com/abelweiwencai
+@software: PyCharm Community Edition
+@file: tests.py
+@time: 2017/7/15 14:19
+"""
+
+import datetime
+from django.utils import timezone
 from django.test import TestCase
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTest(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is in future
+        :return:
+        """
+        tmp_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=tmp_time)
+        self.assertIs(future_question.is_published_recently(), False)
