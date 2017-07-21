b05c637f30c3ae4f06588737d8f5ab620ea698df
vidweiwc <972237007@qq.com>
2017-07-21 15:52:28 +0800

    Add buildout and it can run

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
-    name = "testbuildout",
+    name = "mydjango",
     version = "0.0.1",
-    url = "https://github.com/abelweiwencai/learngit.git",
+    url = "https://github.com/abelweiwencai/mydjango.git",
     license = 'BSD',
     description = "A short test for use biuldout in django",
     author = 'Abel Wei',
-    packages = find_packages('wei'),
-    package_dir = {'': 'wei'},
+    packages = find_packages('weiwc'),
+    package_dir = {'': 'weiwc'},
     install_requires = ['setuptools']
 )
