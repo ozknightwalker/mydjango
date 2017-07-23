adce6d0edc342aad30124a859d1e25a2ad730233
weiwc <972237007@qq.com>
2017-07-23 23:06:14 +0800

    Add hnews project

--- weiwc/polls/admin.py
+++ weiwc/polls/admin.py
@@ -7,5 +7,14 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Choice)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
